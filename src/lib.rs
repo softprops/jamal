dc35f6305c53a1409617393b6fce92631809d5c5
softprops <d.tangren@gmail.com>
2016-06-17 00:30:29 -0400

    increase cov

--- src/lib.rs
+++ src/lib.rs
@@ -158,4 +158,17 @@ mod tests {
         assert_eq!(to_json(&input).unwrap(), output);
     }
 
+    #[test]
+    fn json_bool_to_yaml() {
+        let input = serde_json::to_value(&true);
+        let output = serde_yaml::Value::Boolean(true);
+        assert_eq!(to_yaml(&input).unwrap(), output);
+    }
+
+    #[test]
+    fn yaml_bool_to_json() {
+        let input = serde_yaml::to_value(&true);
+        let output = serde_json::Value::Bool(true);
+        assert_eq!(to_json(&input).unwrap(), output);
+    }
 }
